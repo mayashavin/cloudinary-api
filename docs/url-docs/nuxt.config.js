e387c26fab96dbc3263d1b8cd41e20844de45b0d
Maya Shavin <maya@cloudinary.com>
2021-02-04 10:41:32 +0200

    chore: update goodle verification

--- docs/url-docs/nuxt.config.js
+++ docs/url-docs/nuxt.config.js
@@ -1,6 +1,11 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+  head: {
+    meta: [
+      { name: 'google-site-verification', content: "VqBuI6LAOEm-RHyKbbLnmhReR-Tk04T9foBKmr9m_LM" }
+    ]
+  },
   docs: {
     primaryColor: '#3448c5'
   },
