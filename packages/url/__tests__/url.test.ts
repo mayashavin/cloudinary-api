084f85d9c59efd5e8dae9df3ac8623f92d5c7cc1
Greg Poole <greg.poole@protonmail.com>
2023-02-22 08:28:02 +1100

    Add test to validate filenames with no extension are handled

--- packages/url/__tests__/url.test.ts
+++ packages/url/__tests__/url.test.ts
@@ -210,6 +210,10 @@ describe('Url', () => {
       expect(extractPublicId('example')).toEqual('example')
     })
 
+    it('should correctly handle filenames with no extension', () => {
+      expect(extractPublicId('https://res.cloudinary.com/xyz/image/upload/v111111111/Folder/noextension')).toBe('Folder/noextension')
+    })
+
     it('should allow multiple dots in the filename before the extension', () => {
       expect(extractPublicId('https://res.cloudinary.com/xyz/image/upload/v111111111/Folder/Name.With.Multiple.Dots.png')).toBe('Folder/Name.With.Multiple.Dots')
     })
